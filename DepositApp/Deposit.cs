32e26b1233dea737a518429ac64190a8cc3c1ad3
sunny14 kk <faina.private@gmail.com>
2023-12-18 18:35:06 +0200

    Add project files.

---
+++ DepositApp/Deposit.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepositApp
+{
+    class Deposit
+    {
+        //the amount of money in the deposit on its start
+        private int InitialAmount;
+
+        //annual revenue in %
+        //Example: Revenue = 7 stands for 7% revenue in a year
+        private int AnnualRevenue;
+
+        public Deposit()
+        {
+            InitialAmount = 1000;
+            AnnualRevenue = 7;
+        }
+
+        public Deposit(int amount, int revenue)
+        {
+            InitialAmount = amount;
+            AnnualRevenue = revenue;
+        }
+
+        public int getDepositAfter(int years)
+        {
+            int total = InitialAmount;
+            do
+            {
+                total = total + total * AnnualRevenue / 100;
+                years--;
+            } while (years > 0);
+
+                return total;
+        }
+
+        public void ShowProgress(int years)
+        {
+            int total = InitialAmount;
+            Console.Write(total);
+
+            do
+            {
+                total = total + total * AnnualRevenue / 100;
+                years--;
+                Console.Write(","+total);
+            } while (years > 0);
+
+            Console.WriteLine("\n");
+        }  
+
+    }
+}
---
+++ DepositApp/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+namespace DepositApp
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Deposit d = new Deposit();
+            d.ShowProgress(10);
+        }
+    }
+
+}
