d519e617f39936e73605a8d610257d5d736aecd1
sunny14 kk <faina.private@gmail.com>
2023-12-20 09:37:32 +0200

    after training

--- DepositApp/Deposit.cs
+++ DepositApp/Deposit.cs
@@ -6,47 +6,64 @@ using System.Threading.Tasks;
 
 namespace DepositApp
 {
-    class Deposit
+    public class Deposit
     {
         //the amount of money in the deposit on its start
-        private int InitialAmount;
+        private int initialAmount;
 
         //annual revenue in %
-        //Example: Revenue = 7 stands for 7% revenue in a year
-        private int AnnualRevenue;
+        //Example: annualRevenue = 7 stands for 7% revenue in a year
+        private int annualRevenue;
 
+        /*
+         * default constructor
+         * sets initial deposit to 1000 and annual revenue to 7%
+         */
         public Deposit()
         {
-            InitialAmount = 1000;
-            AnnualRevenue = 7;
+            initialAmount = 1000;
+            annualRevenue = 7;
         }
 
+        /*
+         * constructor
+         * gets initial amount and annual revenue and sets them to deposit
+         */
         public Deposit(int amount, int revenue)
         {
-            InitialAmount = amount;
-            AnnualRevenue = revenue;
+            initialAmount = amount;
+            annualRevenue = revenue;
         }
 
+        /*
+         * gets number of years
+         * returns a deposit after these years
+         * example: getDepositAfter(10) returns how much money will be in the deposit after 10 years
+         */
         public int getDepositAfter(int years)
         {
-            int total = InitialAmount;
+            int total = initialAmount;
             do
             {
-                total = total + total * AnnualRevenue / 100;
+                total = total + total * annualRevenue / 100;
                 years--;
             } while (years > 0);
 
-                return total;
+            return total;
         }
 
+        /*
+         * gets number of years
+         * prints ho much money will be in the deposit in each year
+         */
         public void ShowProgress(int years)
         {
-            int total = InitialAmount;
+            int total = initialAmount;
             Console.Write(total);
 
             do
             {
-                total = total + total * AnnualRevenue / 100;
+                total = total + total * annualRevenue / 100;
                 years--;
                 Console.Write(","+total);
             } while (years > 0);
--- DepositApp/Program.cs
+++ DepositApp/Program.cs
@@ -7,8 +7,14 @@ namespace DepositApp
     {
         public static void Main(String[] args)
         {
-            Deposit d = new Deposit();
-            d.ShowProgress(10);
+            Deposit d = new Deposit(1000, 7);
+
+            int years = 10;
+            int depositAfterTenYears = d.getDepositAfter(years);
+            Console.WriteLine("After "+years+" years I will have "+depositAfterTenYears+"\n");
+
+            Console.WriteLine("How much money will I have in each year? Here is the answer:");
+            d.ShowProgress(years);
         }
     }
 
